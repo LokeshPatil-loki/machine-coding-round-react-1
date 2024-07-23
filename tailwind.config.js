945f80a7d2bcb3600e5a23aebbb39c7f377de715
LokeshPatil-loki <lokesh1404@outlook.com>
2024-07-24 01:59:12 +0530

    added required colors in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,12 @@ export default {
       colors: {
         success: '#23CF9B',
         error: '#EB2D5B',
+        'mint-green': '#D2E3C8',
+        'forest-green': '#4F6F52',
+        dark: '#4B4747',
+        'slate-blue': '#6C6BAF',
+        'pale-mint-green': '#DBFFCE',
+        subtitle: '#4B4747',
       },
     },
   },
